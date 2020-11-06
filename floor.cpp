5fdb18f569b3c54a6a80f3e88bc31da041c9dcc6
Jane Smith <jame.smith@mths.ca>
2020-11-06 04:06:31 +0000

    init

---
+++ floor.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by Mr. Coxall
+// Created on June 2020
+// This program uses libraries
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // this function returns the integer portion of a float
+    std::cout << "The integer part of 12.9 is " << (floor(12.9)) << std::endl;
+}

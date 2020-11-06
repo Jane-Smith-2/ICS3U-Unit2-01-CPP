2c5ef6f44e590969f503443bf230d390069ef6b9
Jane-Smith-2 <jame.smith@mths.ca>
2020-11-06 04:11:34 +0000

    fixed comment

--- floor.cpp
+++ floor.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by Mr. Coxall
-// Created on June 2020
+// Created on Nov 2020
 // This program uses libraries
 
 #include <iostream>
